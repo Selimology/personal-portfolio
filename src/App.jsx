461eff4923ec9df038e06c29ea7ceb7d9f891b0e
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-10 23:49:34 +0200

    fix: add scroll to top on route change
    The error occurred when changing from one page to another using react-router-dom. If user has visited that page previously and scrolled down, when re-visited it continues from there.
    
    ScrollToTop is not supported by all Browsers.
    It doesn't work on Mozilla. Works on Google Chrome.
    
    Solution Approach:
    https://stackoverflow.com/questions/71717114/scroll-to-top-on-every-transition-react-router-dom-v6
    https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, About } from './pages/index';
+import { Home, About, Contact } from './pages/index';
 import { Header, Footer } from './components/index';
-
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col h-screen">
-        <Header />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="about" element={<About />} />
-          </Routes>
-        </div>
-        <Footer />
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
