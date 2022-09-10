9ac08e1d7ecc2dd41ce891f58428a56c699373d3
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-10 13:55:54 +0200

    refactor: export multiple components from index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Header } from './Header';
 export { default as Scroller } from './Scroller';
+export { default as Notification } from './Notification';
+export { default as Footer } from './Footer';
