8c9351a42ef681edb32cc8db17e42f765b6ecb1b
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-10 18:57:05 +0200

    chore: import images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,41 @@
-import react from './react.svg';
+import logo from './react.svg';
 import profilephoto from './profilephoto.png';
+import angular from './angular.png';
+import css from './css.png';
+import html from './html.png';
+import javascript from './javascript.png';
+import graphql from './graphql.png';
+import git from './git.png';
+import react from './react.png';
+import mu5 from './mu5.png';
+import tailwind from './tailwind.png';
+import typescript from './typescript.png';
+import sass from './sass.png';
+import redux from './redux.png';
+import email from './email.png';
+import github from './github.png';
+import linkedin from './linkedin.png';
+import youtube from './youtube.png';
+import aboutphoto from './aboutphoto.jpg';
 
-export { react, profilephoto };
+export {
+  logo,
+  profilephoto,
+  angular,
+  css,
+  html,
+  javascript,
+  graphql,
+  git,
+  react,
+  mu5,
+  tailwind,
+  typescript,
+  sass,
+  redux,
+  email,
+  github,
+  linkedin,
+  youtube,
+  aboutphoto,
+};
