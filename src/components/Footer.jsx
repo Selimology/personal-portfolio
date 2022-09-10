79882d109f6f767cb123b635750469e264c56c30
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-10 23:51:56 +0200

    chore: import component from other folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import Notification from './Notification';
 
 function Footer() {
   return (
