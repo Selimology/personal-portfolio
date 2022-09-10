14f7c62e2b19e740ab95c73f7905c42932d04979
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-09-10 23:51:29 +0200

    refactor: gather all data in single file

---
+++ src/components/Data.jsx
@@ -0,0 +1,139 @@
+import {
+  react,
+  angular,
+  css,
+  html,
+  javascript,
+  graphql,
+  git,
+  mu5,
+  tailwind,
+  typescript,
+  sass,
+  redux,
+  github,
+  linkedin,
+  email,
+  youtube,
+} from '../assets';
+
+export const Projectsdata = [
+  {
+    title: 'Project 1',
+    Category: 'Web App',
+    thumbnail:
+      'https://images.unsplash.com/photo-1657664065994-5e257c88b7f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80',
+    alt: 'Project1 description descriptiondescriptiondescription',
+    githublink: '#',
+    websitelink: '#',
+    technologies: 'React, Node, Express, MongoDB, Redux, Tailwind,',
+  },
+  {
+    title: 'Project 2',
+    Category: 'Web App',
+    thumbnail:
+      'https://images.unsplash.com/photo-1657664065994-5e257c88b7f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80',
+    alt: 'Project1 description',
+    githublink: '#',
+    websitelink: '#',
+    technologies: 'React, Node, Express, MongoDB',
+  },
+  {
+    title: 'Project 3',
+    Category: 'Mobile App',
+    thumbnail:
+      'https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    alt: 'Project1 description',
+    githublink: '#',
+    websitelink: '#',
+    technologies: 'React, Node, Express, MongoDB',
+  },
+  {
+    title: 'Project 4',
+    thumbnail:
+      'https://images.unsplash.com/photo-1545796228-ed5ce0f83fcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1056&q=80',
+    Category: 'Web App',
+    alt: 'Project1 description',
+    githublink: '#',
+    websitelink: '#',
+    technologies: 'React, Node, Express, MongoDB',
+  },
+];
+
+export const Skillsdata = [
+  {
+    name: 'React',
+    icon: react,
+  },
+  {
+    name: 'Angular',
+    icon: angular,
+  },
+  {
+    name: 'Css',
+    icon: css,
+  },
+  {
+    name: 'Html',
+    icon: html,
+  },
+  {
+    name: 'Javascript',
+    icon: javascript,
+  },
+  {
+    name: 'Graphql',
+    icon: graphql,
+  },
+  {
+    name: 'Git',
+    icon: git,
+  },
+  {
+    name: 'Mu5',
+    icon: mu5,
+  },
+  {
+    name: 'Tailwind',
+    icon: tailwind,
+  },
+  {
+    name: 'Typescript',
+    icon: typescript,
+  },
+  {
+    name: 'Sass',
+    icon: sass,
+  },
+  {
+    name: 'Redux',
+    icon: redux,
+  },
+];
+
+export const Socialsdata = [
+  {
+    name: 'Github',
+    icon: github,
+    alt: 'Fork me on Github',
+    link: 'https://github.com/Selimology',
+  },
+  {
+    name: 'Email',
+    icon: email,
+    alt: 'Email me',
+    link: 'mailto:kertekin.business@gmail.com',
+  },
+  {
+    name: 'Linkedin',
+    icon: linkedin,
+    alt: 'Connect with me on Linkedin',
+    link: 'https://www.linkedin.com/in/kamil-ertekin-64a7a9208/',
+  },
+  {
+    name: 'Youtube',
+    icon: youtube,
+    alt: 'Subscribe to my Youtube channel',
+    link: 'https://www.youtube.com/channel/UCdibH_hwX-xAy_FGJCoTkzg',
+  },
+];
